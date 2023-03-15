654ceeb56f102413f2d33278d527c6973529d000
hyorimm <hyorimm@gmail.com>
2023-03-15 21:45:19 +0900

    feat: 툴팁 커스텀

---
+++ src/components/CustomTooltip.tsx
@@ -0,0 +1,37 @@
+import { IProps } from '@/interface/props';
+
+const CustomTooltip = ({ active, payload, setDot }: IProps) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <h3
+          className="label"
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >{`🚩${payload[0].payload.id}`}</h3>
+        <div>
+          {payload.map((item) => (
+            <div key={item.dataKey} className="tooltip-container">
+              <div className="tooltip-inner">
+                <div
+                  className="tooltip-content-value"
+                  style={{
+                    color: item.color,
+                  }}
+                >
+                  {item.value}
+                </div>
+                <div className="tooltip-content-datakey">{item.dataKey}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  return null;
+};
+
+export default CustomTooltip;
